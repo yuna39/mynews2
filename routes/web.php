2cdfc46eef8f1f94ba10979094d49b7535592bdf
粟野 <yuuuuuna39@gmail.com>
2021-07-04 22:13:30 +0000

    PHP/Laravel15確認エラー

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
     Route::post('news/create', 'Admin\NewsController@create');
+    Route::get('news', 'Admin\NewsController@index')->middleware('auth');
 });
 
 // profile
